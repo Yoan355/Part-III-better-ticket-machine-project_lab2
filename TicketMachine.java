459d9dc241e0fc31b40acdf77cb745f2b97a8c62
Yoan355 <joantodri@gmail.com>
2025-09-05 19:34:40 -0400

    Q58-Q61.

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,6 +17,29 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+     
+    //Q57,Q58
+    public void affordable(int budget)  {
+    if (price > budget ) {
+        System.out.println(""+ budget +" Too expensive");
+    } else {
+        System.out.println(""+ budget +" Just right");
+    }
+}
+    
+     public int emptyMachine() {
+         
+       int amountEmptied= total;
+        
+       
+       total=0;
+        
+        return amountEmptied;
+                  
+     
+     }
+
+
 
     /**
      * Create a machine that issues tickets of the given price.
@@ -65,8 +88,13 @@ public class TicketMachine
      * an error message if more money is required.
      */
     public void printTicket()
+    
+    
     {
+    
         if(balance >= price) {
+            
+        
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
@@ -84,14 +112,17 @@ public class TicketMachine
     }
 
     /**
-     * Return the money in the balance.
+     * Return the money in the balance.   
      * The balance is cleared.
      */
     public int refundBalance()
     {
         int amountToRefund;
-        amountToRefund = balance;
-        balance = 0;
+        amountToRefund = balance; 
+        balance = 0; 
         return amountToRefund;
+        
+        
+    
     }
 }
